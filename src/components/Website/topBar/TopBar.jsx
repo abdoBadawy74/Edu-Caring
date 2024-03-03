eb5fda7b03a723db025d0115a682e31ce8363722
abd074 <badawyabdulrahman099@gmail.com>
2024-03-03 04:17:21 +0200

    شوية تعديلات في الطرش

--- src/components/Website/topBar/TopBar.jsx
+++ src/components/Website/topBar/TopBar.jsx
@@ -1,19 +1,45 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
 import Language from "./Language";
 import Support from "./Support";
 import Notification from "./Notification";
-import Person from './Person';
+import Person from "./Person";
+import { useLocation } from "react-router-dom";
+
 export default function TopBar() {
+  const location = useLocation();
+  const [routeText, setRouteText] = useState("Home");
+
+  // Use the useEffect hook to change the text when the route changes
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/home":
+        setRouteText("Home");
+        break;
+      case "/myevents":
+        setRouteText("My Events");
+        break;
+      case "/community":
+        setRouteText("Community");
+        break;
+      case "/profile":
+        setRouteText("Profile");
+        break;
+      default:
+        setRouteText("Home");
+        break;
+    }
+  }, [location]);
+
   return (
     <>
-      <div className="topbar d-flex justify-content-between align-items-center px-2 pt-2" >
-        <span>Home</span>
+      <div className="topbar d-flex justify-content-between align-items-center px-2 pt-2">
+        <span>{routeText}</span>
 
         <div className="d-flex align-items-center gap-4">
-          <Language></Language>
-          <Support></Support>
-          <Notification></Notification>
-          <Person></Person>
+          <Language />
+          <Support />
+          <Notification />
+          <Person />
         </div>
       </div>
     </>
